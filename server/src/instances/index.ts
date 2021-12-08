af199624d354333c1c3e9cebebcd9837295500bb
Iago <iago.idom@gmail.com>
2021-12-08 10:26:58 -0300

    sequelchanges

--- server/src/instances/index.ts
+++ server/src/instances/index.ts
@@ -2,10 +2,18 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
+// export const sequelize = new Sequelize(
+//   process.env.PG_DB as string,
+//   process.env.PG_USER as string,
+//   process.env.PG_PASSWORD as string,
+//   {
+//     dialect: 'postgres',
+//     port: +(process.env.PG_PORT as string)
+//   }
+// );
+
 export const sequelize = new Sequelize(
-  process.env.PG_DB as string,
-  process.env.PG_USER as string,
-  process.env.PG_PASSWORD as string,
+  "postgres://qsqhpheyxyrnrr:cc5f2a6dfb70a4f948e0efea216efd0f05a0f4352a256fdbd2a28ce8c50d458d@ec2-35-171-90-188.compute-1.amazonaws.com:5432/dan6jonjkdn2mu",
   {
     dialect: 'postgres',
     port: +(process.env.PG_PORT as string)
--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -4,15 +4,15 @@ const router = Router();
 import auth from '../controllers/auth';
 
 router.get('/categories', apiController.categories);
-router.get('/products', apiController.products);
+// router.get('/products', apiController.products);
 router.get('/cat', apiController.createCategories);
-router.get('/prod', apiController.createProducts);
-router.post('/auth/signup', apiController.signUp);
-router.post('/auth/login', apiController.login);
-router.post('/auth/newProfile', auth, apiController.newProfile);
-router.post('/address', apiController.address);
-router.post('/newOrder', apiController.newOrder);
-router.post('/orders', auth, apiController.orders);
-router.post('/payments', auth, apiController.stripePayment);
+// router.get('/prod', apiController.createProducts);
+// router.post('/auth/signup', apiController.signUp);
+// router.post('/auth/login', apiController.login);
+// router.post('/auth/newProfile', auth, apiController.newProfile);
+//router.post('/address', apiController.address);
+//router.post('/newOrder', apiController.newOrder);
+//router.post('/orders', auth, apiController.orders);
+//router.post('/payments', auth, apiController.stripePayment);
 
 export default router;
